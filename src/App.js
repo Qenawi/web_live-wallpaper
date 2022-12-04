f4fdfbca473c7761653d924d751e739f19f3b30f
ahmed mohmaed Qenawi <ahmuibrahim@banquemisr.com>
2022-12-04 10:31:42 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import sample from './vedio/bg.mp4';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay loop muted id='bg-video'
+      src={sample}
+      />
     </div>
   );
 }
